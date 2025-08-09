7517bc541d99eaf81956fda590b5bf6e4b0737ce
Benjam-byte <benjamin99.mitton@gmail.com>
2025-08-09 18:03:32 +0200

    fix eploration layout

--- src/app/core/components/info-footer/info-footer.component.ts
+++ src/app/core/components/info-footer/info-footer.component.ts
@@ -8,6 +8,8 @@ import { GameEngineService } from '../../service/game-engine.service';
 import { ActionGaugeComponent } from './action-gauge/action-gauge.component';
 import { CommonModule } from '@angular/common';
 
+type InformationMode = 'fight' | 'loot' | 'world' | 'monster';
+
 @Component({
   selector: 'app-info-footer',
   imports: [ActionGaugeComponent, CommonModule],
@@ -22,4 +24,11 @@ export class InfoFooterComponent {
   travelDuration = this.gameEngineService.human().travellingSpeed;
   fightingCountDown$ = this.gameEngineService.getFightingCountDown$();
   fightingDuration = this.gameEngineService.human().fightingSpeed;
+
+  infoMode: InformationMode = 'fight';
+
+  updateInfoMode(mode: InformationMode) {
+    console.log(mode);
+    this.infoMode = mode;
+  }
 }
