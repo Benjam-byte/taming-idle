6ecc3dbb8e8b7029c6d6a20f3a6cb175749fb463
Benjam-byte <benjamin99.mitton@gmail.com>
2025-09-24 18:07:53 +0200

    fix gauge animation

--- src/app/core/components/info-footer/action-gauge/action-gauge.component.ts
+++ src/app/core/components/info-footer/action-gauge/action-gauge.component.ts
@@ -1,32 +1,49 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectorRef,
-  Component,
-  computed,
-  inject,
-  input,
-} from '@angular/core';
+import { Component, computed, effect, input, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-action-gauge',
-  imports: [CommonModule],
-  templateUrl: './action-gauge.component.html',
-  styleUrl: './action-gauge.component.scss',
+    selector: 'app-action-gauge',
+    imports: [CommonModule],
+    templateUrl: './action-gauge.component.html',
+    styleUrl: './action-gauge.component.scss',
 })
 export class ActionGaugeComponent {
-  durationMs = input<number>(1000);
-  progress = input<number>(0);
-  isGreen = input<boolean>(false);
+    durationMs = input<number>(1000);
+    initValue = input<number>(0);
+    isGreen = input<boolean>(false);
 
-  barStyle = computed(() => {
-    return {
-      height: `${this.normalizeToPercent(this.progress(), this.durationMs())}%`,
-    };
-  });
+    valuebar = signal(0);
+    intervalId!: any | null;
 
-  private normalizeToPercent(x: number, durationInMs: number): number {
-    if (durationInMs <= 0) return 100;
-    const filled = 1 - x / durationInMs;
-    return Math.max(0, Math.min(100, filled * 100));
-  }
+    progressUpdate = effect(() => {
+        console.log(this.initValue());
+        if (this.initValue() === 950) {
+            console.log('set interval', this.intervalId);
+            if (!this.intervalId) {
+                this.intervalId = setInterval(() => {
+                    this.valuebar.update((v) => v + this.durationMs() / 100);
+                    if (this.valuebar() >= this.durationMs()) {
+                        if (!this.intervalId) return;
+                        clearInterval(this.intervalId);
+                        this.valuebar.set(0);
+                        this.intervalId = null;
+                    }
+                }, 10);
+            }
+        }
+    });
+
+    barStyle = computed(() => {
+        return {
+            height: `${this.normalizeToPercent(
+                this.valuebar(),
+                this.durationMs()
+            )}%`,
+        };
+    });
+
+    private normalizeToPercent(x: number, durationInMs: number): number {
+        const filled = 1 - x / durationInMs;
+        return Math.max(0, Math.min(100, filled * 100));
+    }
 }
