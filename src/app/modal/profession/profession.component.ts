aa208d8dee067692d05cab1edc94eaf9c3a68397
Benjam-byte <benjamin99.mitton@gmail.com>
2025-10-12 21:54:53 +0200

    fix modal layout + profession

--- src/app/modal/profession/profession.component.ts
+++ src/app/modal/profession/profession.component.ts
@@ -2,20 +2,15 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ProfessionCardComponent } from './profession-card/profession-card.component';
 import { ProfessionManagerService } from 'src/app/core/service/player/profession-manager.service';
 import { CommonModule } from '@angular/common';
-import { IonContent, ModalController } from '@ionic/angular/standalone';
+import { ModalLayoutComponent } from '../modal-layout/modal-layout.component';
 
 @Component({
     selector: 'app-profession',
-    imports: [IonContent, ProfessionCardComponent, CommonModule],
+    imports: [ProfessionCardComponent, CommonModule, ModalLayoutComponent],
     templateUrl: './profession.component.html',
     styleUrl: './profession.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfessionComponent {
     professionManager = inject(ProfessionManagerService);
-    modalCtrl = inject(ModalController);
-
-    close() {
-        this.modalCtrl.dismiss();
-    }
 }
