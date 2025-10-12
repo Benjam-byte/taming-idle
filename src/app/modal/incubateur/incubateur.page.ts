bca1487d2ae27c025609b9eb61f6d6bc56c4fae9
Benjam-byte <benjamin99.mitton@gmail.com>
2025-10-12 21:39:14 +0200

    modal layout for incubateur

--- src/app/modal/incubateur/incubateur.page.ts
+++ src/app/modal/incubateur/incubateur.page.ts
@@ -1,20 +1,19 @@
 import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, ModalController } from '@ionic/angular/standalone';
 import { EggManagerService } from 'src/app/core/service/monster/egg-manager.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Egg } from 'src/app/database/egg/egg.type';
 import { FormatDuration } from '../../core/pipe/formatDuration.pipe';
+import { ModalLayoutComponent } from '../modal-layout/modal-layout.component';
 
 @Component({
     selector: 'app-incubateur',
     templateUrl: './incubateur.page.html',
     styleUrls: ['./incubateur.page.scss'],
     standalone: true,
-    imports: [IonContent, CommonModule, FormatDuration],
+    imports: [CommonModule, FormatDuration, ModalLayoutComponent],
 })
 export class IncubateurPage {
-    modalCtrl = inject(ModalController);
     eggManagerService = inject(EggManagerService);
 
     eggList = toSignal(this.eggManagerService.eggList$);
@@ -23,9 +22,4 @@ export class IncubateurPage {
         if (!eggList) return Array(24).fill(null);
         return [...eggList, ...Array(24 - eggList.length).fill(null)];
     });
-
-    close() {
-        console.log(this.eggList());
-        this.modalCtrl.dismiss();
-    }
 }
--- src/app/modal/modal-layout/modal-layout.component.ts
+++ src/app/modal/modal-layout/modal-layout.component.ts
@@ -1,5 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    input,
+} from '@angular/core';
 import { IonContent, ModalController } from '@ionic/angular/standalone';
+import { MenuComponent } from '../menu/menu.component';
 @Component({
     selector: 'app-modal-layout',
     imports: [IonContent],
@@ -9,8 +15,21 @@ import { IonContent, ModalController } from '@ionic/angular/standalone';
 })
 export class ModalLayoutComponent {
     modalCtrl = inject(ModalController);
+    title = input.required();
 
     close() {
         this.modalCtrl.dismiss();
+    }
+
+    async goToMenu() {
+        this.close();
+        const modal = await this.modalCtrl.create({
+            component: MenuComponent,
+            cssClass: 'full-screen-modal',
+            backdropDismiss: true,
+            showBackdrop: true,
+        });
+
+        modal.present();
     }
 }
