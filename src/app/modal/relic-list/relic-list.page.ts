e3fde4fc8ac5c30a0a454327e49ef9875e4e4914
Benjam-byte <benjamin99.mitton@gmail.com>
2025-10-12 22:35:47 +0200

    relic use modal layout

--- src/app/modal/relic-list/relic-list.page.ts
+++ src/app/modal/relic-list/relic-list.page.ts
@@ -1,28 +1,23 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, ModalController } from '@ionic/angular/standalone';
 import { RelicManagerService } from 'src/app/core/service/player/relic-manager.service';
 import { Relics } from 'src/app/database/relics/relics.type';
 import { HumanManagerService } from 'src/app/core/service/player/human-manager.service';
+import { ModalLayoutComponent } from '../modal-layout/modal-layout.component';
 
 @Component({
     selector: 'app-relic-list',
     templateUrl: './relic-list.page.html',
     styleUrls: ['./relic-list.page.scss'],
     standalone: true,
-    imports: [IonContent, CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, ModalLayoutComponent],
 })
 export class RelicListPage {
-    modalCtrl = inject(ModalController);
     relicManagerService = inject(RelicManagerService);
     humanManagerService = inject(HumanManagerService);
 
     readonly selectedId = signal<string | undefined>(undefined);
-
-    close() {
-        this.modalCtrl.dismiss();
-    }
 
     select(relic: Relics) {
         if (relic.id === this.selectedId()) {
