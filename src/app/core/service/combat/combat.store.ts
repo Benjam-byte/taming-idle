359433e545fce6a6185dce0e1598ccf306de6290
BenjamByte <benjamin99.mitton@gmail.com>
2026-07-02 11:35:17 +0200

    ✨ WIP

--- src/app/core/service/combat/combat.store.ts
+++ src/app/core/service/combat/combat.store.ts
@@ -1,6 +1,15 @@
 import { computed } from '@angular/core';
-import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
-import { Monster } from './monster';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { Monster, Stat } from './monster';
+import { MonsterDefintion } from 'src/app/config/type/monster-type';
+import { MonsterDict } from 'src/app/config/monster';
+import { pick3WeightedItem } from '../../helpers/choice/picked-weight';
 
 type CombatState = {
   isCombat: boolean;
@@ -21,47 +30,69 @@ const initialState: CombatState = {
 export const CombatStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withComputed(({ isCombat, playerTurn, isTurnResolving, monster, player }) => ({
-    isMonsterAlive: computed(() => monster()?.isAlive ?? false),
-    monsterLife: computed(() => monster()?.life ?? 0),
-    monsterMaxLife: computed(() => monster()?.maxLife ?? 0),
-    isPlayerAlive: computed(() => player()?.isAlive ?? false),
-    playerLife: computed(() => player()?.life ?? 0),
-    playerMaxLife: computed(() => player()?.maxLife ?? 0),
-    canPlayerAttack: computed(
-      () =>
-        isCombat() &&
-        playerTurn() &&
-        !isTurnResolving() &&
-        (monster()?.isAlive ?? false) &&
-        (player()?.isAlive ?? false),
-    ),
-    shouldMonsterAttack: computed(
-      () =>
-        isCombat() &&
-        !playerTurn() &&
-        !isTurnResolving() &&
-        (monster()?.isAlive ?? false) &&
-        (player()?.isAlive ?? false),
-    ),
-  })),
+  withComputed(
+    ({ isCombat, playerTurn, isTurnResolving, monster, player }) => ({
+      isMonsterAlive: computed(() => monster()?.isAlive ?? false),
+      monsterLife: computed(() => monster()?.stat.hp ?? 0),
+      monsterMaxLife: computed(() => monster()?.life ?? 0),
+      isPlayerAlive: computed(() => player()?.isAlive ?? false),
+      playerLife: computed(() => player()?.stat.hp ?? 0),
+      playerMaxLife: computed(() => player()?.life ?? 0),
+      canPlayerAttack: computed(
+        () =>
+          isCombat() &&
+          playerTurn() &&
+          !isTurnResolving() &&
+          (monster()?.isAlive ?? false) &&
+          (player()?.isAlive ?? false),
+      ),
+      shouldMonsterAttack: computed(
+        () =>
+          isCombat() &&
+          !playerTurn() &&
+          !isTurnResolving() &&
+          (monster()?.isAlive ?? false) &&
+          (player()?.isAlive ?? false),
+      ),
+    }),
+  ),
   withMethods((store) => ({
     startCombat(): void {
+      this.spawnMonster('Slime');
       patchState(store, {
         isCombat: true,
-        playerTurn: true,
+        playerTurn: store.player()!.stat.speed >= store.monster()!.stat.speed,
         isTurnResolving: false,
-        monster: Monster.create(false, 1),
-        player: Monster.create(false, 1),
       });
     },
     endCombat(): void {
       patchState(store, { ...initialState });
     },
-    spawnMonster(isEnchanted: boolean, monsterLevel: number): void {
+    spawnMonster(monsterName: string): void {
+      const monsterDef = MonsterDict.find(
+        (monsterDef) => monsterDef.name === monsterName,
+      );
+      if (!monsterDef) return;
+      const playerDef = MonsterDict.find(
+        (monsterDef) => monsterDef.name === 'Terra larva',
+      );
+      if (!playerDef) return;
+
+      const monsterStat: Stat = { hp: 10, defense: 0, attack: 1, speed: 0 };
+      const playerStat: Stat = { hp: 10, defense: 0, attack: 1, speed: 1 };
+      const attackSpeKey = pick3WeightedItem(monsterDef.attackSpeList);
+
       patchState(store, {
-        monster: Monster.create(isEnchanted, monsterLevel),
-        player: Monster.create(false, 1),
+        monster: Monster.create(
+          monsterStat,
+          monsterDef.baseAttack,
+          attackSpeKey,
+        ),
+        player: Monster.create(
+          playerStat,
+          monsterDef.baseAttack,
+          monsterDef.attackSpeList[0],
+        ),
       });
     },
     hitMonster(damage: number): void {
--- src/app/core/service/combat/monster.ts
+++ src/app/core/service/combat/monster.ts
@@ -1,27 +1,57 @@
+import { AttackSpeKey, BaseAttackKey } from 'src/app/config/attack';
+
+export type Stat = {
+  hp: number;
+  attack: number;
+  defense: number;
+  speed: number;
+};
+
 export class Monster {
   constructor(
     public readonly life: number,
-    public readonly maxLife: number,
+    public readonly shield: number,
+    public readonly stat: Stat,
     public readonly type: string,
-    public readonly isEnchanted: boolean,
+    public readonly baseAttack: BaseAttackKey,
+    public readonly attackStocked: number,
+    public readonly attackSpe: AttackSpeKey,
     public readonly isAlive: boolean,
   ) {}
 
-  static create(isEnchanted: boolean, monsterLevel: number): Monster {
-    const maxLife = 6 + monsterLevel * 3;
-
-    return new Monster(maxLife, maxLife, "slime", isEnchanted, true);
+  static create(
+    stat: Stat,
+    baseAttack: BaseAttackKey,
+    attackSpe: AttackSpeKey,
+  ): Monster {
+    const maxLife = stat.hp;
+    return new Monster(
+      maxLife,
+      0,
+      stat,
+      'neutre',
+      baseAttack,
+      0,
+      attackSpe,
+      true,
+    );
   }
 
   getHit(damage: number): Monster {
     const nextLife = Math.max(0, this.life - damage);
+    const nextAttackStocked = this.attackStocked + 1;
 
     return new Monster(
       nextLife,
-      this.maxLife,
-      this.type,
-      this.isEnchanted,
+      0,
+      this.stat,
+      'neutre',
+      this.baseAttack,
+      nextAttackStocked,
+      this.attackSpe,
       nextLife > 0,
     );
   }
+
+  getBuffed(buff: ??): Monster {}
 }
